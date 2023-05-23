3a1488a8d63547e54243a22a404a5be7f12f6660
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-05-23 17:31:54 +0300

    -created userModel -bcrypt

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Lotan',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'john',
+      email: 'john@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Slim Shirt',
